02c2a0652c695f674e6c859e3a64a06849815ece
mahfeshar <mahmoudfeshar11@gmail.com>
2024-07-29 16:54:15 +0300

    Refactor API data gathering script and improve task display

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
 """gather data from an API and display completed tasks"""
+import re
 import requests
 import sys
 
-url = 'https://jsonplaceholder.typicode.com/'
+
+url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        employee_id = int(sys.argv[1])
-
-        users_response = requests.get(f'{url}users/{employee_id}')
-
-        user_data = users_response.json()
-        employee_name = user_data['name']
-
-        params = {
-            'userId': employee_id
-        }
-        todos_response = requests.get(f'{url}/todos', params=params)
-        todos_data = todos_response.json()
-
-        completed_tasks = []
-        for todo in todos_data:
-            if todo['completed']:
-                completed_tasks.append(todo['title'])
-
-        print(
-            f"Employee {employee_name} is done with tasks "
-            f"({len(completed_tasks)}/{len(todos_data)}):"
-        )
-
-        for i in completed_tasks:
-            print(f"\t {i}")
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(url, id)).json()
+            todos_res = requests.get('{}/todos'.format(url)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
