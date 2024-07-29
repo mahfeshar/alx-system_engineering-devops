4a249e8d798126074e3546e5d268b9dc793d21ad
mahfeshar <mahmoudfeshar11@gmail.com>
2024-07-29 15:05:13 +0300

    Refactor API data gathering script and improve task display

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python3
+"""gather data from an API and display completed tasks"""
 import requests
 import sys
 
 
-employee_id = sys.argv[1]
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        employee_id = sys.argv[1]
+        url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+        
+        
+        users_response = requests.get(url)
 
-url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-
-users_response = requests.get(url)
-
-user_data = users_response.json()  # dict
-employee_name = user_data['name']
-
-
-url = 'https://jsonplaceholder.typicode.com/todos'
-
-params = {
-    'userId': employee_id
-}
-todos_response = requests.get(url, params=params)
-todos_data = todos_response.json()
-
-done_tasks = 0
-total_tasks = len(todos_data)
-completed_tasks = []
-for todo in todos_data:
-    if todo['completed']:
-        done_tasks += 1
-        completed_tasks.append(todo['title'])
-
-print(
-    f"Employee {employee_name} is done with tasks "
-    f"({done_tasks}/{total_tasks}):"
-)
+        user_data = users_response.json()  # dict
+        employee_name = user_data['name']
 
 
-for i in completed_tasks:
-    print(f"\t {i}")
+        url = 'https://jsonplaceholder.typicode.com/todos'
+
+        params = {
+            'userId': employee_id
+        }
+        todos_response = requests.get(url, params=params)
+        todos_data = todos_response.json()
+
+        done_tasks = 0
+        total_tasks = len(todos_data)
+        completed_tasks = []
+        for todo in todos_data:
+            if todo['completed']:
+                done_tasks += 1
+                completed_tasks.append(todo['title'])
+
+        print(
+            f"Employee {employee_name} is done with tasks "
+            f"({done_tasks}/{total_tasks}):"
+        )
+
+
+        for i in completed_tasks:
+            print(f"\t {i}")
